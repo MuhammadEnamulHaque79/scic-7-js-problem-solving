0bb9f8b3eb153643d7534cc41a27c661fa90f757
MuhammadEnamulHaque79 <muhammad.enamul.haque79@gmail.com>
2023-07-26 22:38:29 +0600

    seconde problem solved

--- problem.js
+++ problem.js
@@ -12,8 +12,124 @@ function reverseString(inputString) {
   }
   const inputString = "Hello, world!";
   const reversed = reverseString(inputString);
-  console.log(reversed);
+//   console.log(reversed);
     
 
-//....................Problem-1 End........................//
+//Problem-2:Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+
+//Input: [2, -5, 10, -3, 7]
+//Output: 19
+
+function sumPositiveNumbers(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        sum += arr[i];
+      }
+    }
+    return sum;
+  }
+  
+  const numbers = [2, -5, 10, -3, 7];
+const result = sumPositiveNumbers(numbers);
+// console.log(result);
+
+
+//Problem-3:Write a JavaScript program to find the most frequent element in an array and return it. 
+
+//Input: [3, 5, 2, 5, 3, 3, 1, 4, 5]
+//Output:3
+
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
+//Problem-2:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+
+//Input: "hello world"
+//Output: "dlrow olleh"
+
+//....................Problem-2 End........................//
 
